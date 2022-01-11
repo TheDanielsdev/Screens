1163bfbd893199d65dd5987ddccb38db37185701
TheDaniels <USER@TheDaniels.thedanielsdev>
2022-01-11 11:15:34 +0100

    second commit

--- lib/screens.dart
+++ lib/screens.dart
@@ -3,8 +3,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatelessWidget {
-  const SignInPage({Key? key}) : super(key: key);
+class Screens extends StatelessWidget {
+  const Screens({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
